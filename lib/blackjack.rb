f5444135e377cfd9570ac3cb32c659cf9b684ddd
Ryan Werner <rmwerner@umich.edu>
2020-07-12 01:50:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,10 +59,11 @@ end
 
 def runner
   welcome
-  total = initial_round
-  while total < 21 do
-    hit?(total)
-    display_card_total(total)
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
-  end_game(total)
+  end_game(card_total)
 end
+
