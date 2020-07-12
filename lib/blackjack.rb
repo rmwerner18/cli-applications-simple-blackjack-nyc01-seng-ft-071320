7c7fa7703fcdea9acea8ba3d94acc29fc0a1c0ed
Ryan Werner <rmwerner@umich.edu>
2020-07-12 01:52:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,23 +35,16 @@ def invalid_command
 end
 
 def hit?(card_total)
-  valid_inputs = ["h", "s"]
-
   prompt_user
-  user_input = get_user_input
-
-  until valid_inputs.include?(user_input)
-    invalid_command
-    prompt_user
-    user_input = get_user_input
-  end
-
-  if user_input == "h"
-    card_total += deal_card
-  end
-  card_total
-end
-
+  input = get_user_input
+if user_input == "h"
+card_total += deal_card
+elsif user_input == "s"
+ card_total
+else
+   invalid_command 
+hit?(card_total)
+ end
 
 #####################################################
 # get every test to pass before coding runner below #
